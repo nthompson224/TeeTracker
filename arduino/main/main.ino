d41d962d7e8334193337c2a84127bf599fa5de9b
nthompson2 <nmt46@uakron.edu>
2025-03-18 23:04:26 -0400

    Refactor comments in main.ino

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -4,7 +4,7 @@
 #include <WebSocketsClient.h>
 #include <ArduinoJson.h>
 
-#include <SparkFun_u-blox_GNSS_Arduino_Library.h>  //http://librarymanager/All#SparkFun_u-blox_GNSS
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
 SFE_UBLOX_GNSS myGNSS;
 
 const char* ssid = "whore home";
@@ -17,12 +17,12 @@ String golferUUID = "";
 
 WebSocketsClient ws;
 
-long lastPing = 0;  //Simple local timer. Limits amount if I2C traffic to u-blox module.
+// local timer to limit the amount of I2C traffic
+long lastPing = 0;
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial)
-    ;  //Wait for user to open terminal
+  while (!Serial);
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -39,17 +39,21 @@ void setup() {
 
   Wire.begin();
 
-  //myGNSS.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+  // Uncomment this line to enable helpful debug messages on Serial
+  //myGNSS.enableDebugging();
 
-  if (myGNSS.begin() == false)  //Connect to the u-blox module using Wire port
+  //Connect to the u-blox module using Wire port
+  if (myGNSS.begin() == false)
   {
     Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
     while (1)
       ;
   }
 
-  myGNSS.setI2COutput(COM_TYPE_UBX);                  //Set the I2C port to output UBX only (turn off NMEA noise)
-  myGNSS.saveConfigSelective(VAL_CFG_SUBSEC_IOPORT);  //Save (only) the communications port settings to flash and BBR
+  //Set the I2C port to output UBX only (turn off NMEA noise)
+  myGNSS.setI2COutput(COM_TYPE_UBX);
+  //Save (only) the communications port settings to flash and BBR
+  myGNSS.saveConfigSelective(VAL_CFG_SUBSEC_IOPORT);
 }
 
 void loop() {
